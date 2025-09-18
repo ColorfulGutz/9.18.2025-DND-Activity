12347aa50ce526dd937f2b78a4d12756ea7bce1a
ColorfulGutz <gavin.bascle@selu.edu>
2025-09-18 13:43:12 -0500

    Hihi

---
+++ src/DNDCharacter.java
@@ -0,0 +1,60 @@
+import java.sql.SQLOutput;
+
+public class DNDCharacter {
+    private String name;
+    private String race;
+    private String characterClass;
+    private int level;
+    private int hitpoints;
+    private int strength;
+    private int dexterity;
+    private int intelligence;
+
+    public DNDCharacter() {
+        this.name = "Unknown";
+        this.race = "Human";
+        this.characterClass = "Fighter";
+        this.level = 1;
+        this.hitpoints = 100;
+        this.strength = 10;
+        this.dexterity = 5;
+        this.intelligence = 3;
+    }
+
+    public DNDCharacter(String name, String race, String characterClass, int level,
+                        int hitpoints, int strength, int dexterity, int intelligence){
+        this.name=name;
+        this.race=race;
+        this.characterClass=characterClass;
+        this.level=level;
+        this.hitpoints=hitpoints;
+        this.strength=strength;
+        this.dexterity=dexterity;
+        this.intelligence=intelligence;
+    }
+
+    public void displayCharacterInfo(){
+        System.out.println("Name: " + this.name);
+        System.out.println("Race: " + this.race);
+        System.out.println("Characters class: " + this.characterClass);
+        System.out.println("Level: " + this.level);
+        System.out.println("Hitpoints: " + this.hitpoints);
+        System.out.println("Strength: " + this.strength);
+        System.out.println("Dexterity: " + this.dexterity);
+        System.out.println("Intelligence: " + this.intelligence);
+        System.out.println();
+    }
+
+    public void takeDamage(int damage){
+        hitpoints -= damage;
+        if (hitpoints < 0){
+            hitpoints = 0;
+        }
+    }
+
+    public void heal(int healing){
+
+    }
+
+
+}
